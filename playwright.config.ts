9d5c0c0b56f088d8eabc0b7962a8c38adb20f333
Vioti <82620421+vinivioti@users.noreply.github.com>
2026-04-27 17:20:38 -0300

    Atualizacao projeto para uso do ambiente de UAT

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-six-zeta.vercel.app',
+    //Versão Produção
+    //baseURL: 'https://velo-six-zeta.vercel.app',
+
+    //Versão UAT
+    baseURL: 'https://velo-3jc4hx3un-viniviotis-projects.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
