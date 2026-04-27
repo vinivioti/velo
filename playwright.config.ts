e457964a6dc5f2069011615e541a23f47b482bd2
Vioti <82620421+vinivioti@users.noreply.github.com>
2026-04-27 16:39:17 -0300

    webserver no pw config + testes unitarios + vercel

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-84epookxn-fernandopapito-4832s-projects.vercel.app',
+    baseURL: 'https://velo-six-zeta.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
